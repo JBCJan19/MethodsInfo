90189ece6087d346010a93ab5216c5eb73e9c254
romielisse <romielisse@gmail.com>
2019-01-23 13:39:13 -0500

    initial commit

---
+++ src/Main2.java
@@ -0,0 +1,15 @@
+public class Main2 {
+    public static void main(String[] args){
+        newMethod();
+        newMethod2();
+
+    }
+
+    public static void newMethod(){
+
+    }
+
+    public static void newMethod2(){
+
+    }
+}
